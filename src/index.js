4ee171b07f8d5a12f88653222b8c45951aa66c92
Wiraa <ur.wiraananda@gmail.com>
2024-01-09 21:45:04 +0800

    get

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT;
+
+app.get("/pasien", async (req, res) => {
+  const allPasien = await prisma.pasien.findMany();
+
+  res.send(allPasien);
+});
 
 app.listen(PORT, () => {
   console.log("Running on " + PORT);
