eb006a276b5ef5def84dd587e125cfbc667789c9
Wiraa <ur.wiraananda@gmail.com>
2024-01-11 15:21:53 +0800

    cors add

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 dotenv.config();
 const PORT = process.env.PORT;
